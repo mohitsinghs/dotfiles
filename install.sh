98b5736da3ef581a50ce7b12f1e08edd432c77b7
Mohit Singh <mohitsinghs.dev@gmail.com>
2018-12-20 15:38:34 +0530

    Add colored output and exit on existance

--- install.sh
+++ install.sh
@@ -1,54 +1,91 @@
 #!/bin/bash
+#
+# automated configuration for macos
+#
+# Author: Mohit Singh
+# https://github.com/mohitsinghs/dotfiles
 
-# exit if macos is not found
-if [[ $(uname) != 'Darwin' ]]; then
-  echo "Sorry! found $(uname) instead of Darwin"
+set -e
+
+success () {
+  printf "\\033[32m ✔ \\033[0m\\n" >&2
+}
+
+fail () {
+  printf "\\n\\033[31m ✖ \\033[0m $1\\n" >&2
   exit 1
-fi
+}
+
+info () {
+  printf "\\033[33m ➜ \\033[0m $1" >&2
+}
 
 # define variables
-DOTLOC=~/Projects/dotfiles
+DOTLOC=$HOME/Projects/dotfiles
 ZSH_USERS=https://github.com/zsh-users
 FUNPATH=/usr/local/share/zsh/site-functions
+
+# exit if macos is not found
+info "Checking system..."
+if [[ $(uname) != 'Darwin' ]]; then
+  fail "You are not on a mac."
+else
+  success
+fi
+
+# exit if dotfiles exists
+info "Looking for dotfiles..."
+if [[ -d  $DOTLOC ]]; then
+  fail "\\033[2m$HOME/dotfiles\\033[0m already exists."
+else
+  success
+fi
 
 # No help from next launch
 touch ~/.hushlogin
 
-echo -n "=> Cloning dotfiles..."
-# cleanup dotfiles directory
-[[ -d $DOTLOC ]] && rm -rf $DOTLOC
 # create dotfiles directory and clone
-mkdir -p $DOTLOC
+info "Cloning dotfiles..."
+[[ ! -d $HOME/Projects ]] && mkdir -p $HOME/Projects
 git clone -q https://github.com/mohitsinghs/dotfiles.git $DOTLOC
-echo "Done"
+success
+
 
 # install homebrew if missing
 if [[ ! -x "$(command -v brew)" ]]; then
-  echo -n "=> Installing homebrew..."
+  info "Installing homebrew...\\n"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  echo "Done"
+else
+  info "Homebrew is already installed.\\n"
 fi
 
-echo -n "=> Installing packages..."
 # install packages
-brew bundle install --file=$DOTLOC/Brewfile > /dev/null
-# install iStats for battery stats
-gem install iStats > /dev/null
-echo "Done"
+info "Installing packages..."
+brew bundle install --file=$DOTLOC/Brewfile >/dev/null
+success
 
-echo -n "=> Linking Files..."
+# install iStats for battery stats
+if [[ ! -x "$(command -v iStats)" ]]; then
+  info "Installing iStats..."
+  gem install iStats > /dev/null
+  success
+else
+  info "iStats is already installed.\\n"
+fi
+
 # link required files
+info "Linking Files..."
 for file in zshrc zshenv gitconfig gitignore
 do
-  rm ~/.$file &> /dev/null
+  rm ~/.$file &>/dev/null
   ln -s $DOTLOC/$file ~/.$file
 done
-echo "Done"
+success
 
 # change default shell to zsh
 chsh -s zsh
 
-echo -n "=> Installing prompt..."
+info "Installing pure prompt..."
 # cleanup old prompt files
 if [[ -x $FUNPATH/prompt_pure_setup && -x $FUNPATH/async ]]; then
   rm -f $FUNPATH/prompt_pure_setup
@@ -59,7 +96,7 @@ curl https://raw.githubusercontent.com/sindresorhus/pure/master/pure.zsh --creat
 curl https://raw.githubusercontent.com/sindresorhus/pure/master/async.zsh -fsSLo $FUNPATH/async
 chmod +x $FUNPATH/prompt_pure_setup
 chmod +x $FUNPATH/async
-echo "Done"
+success
 
 # reload shell
 exec zsh
