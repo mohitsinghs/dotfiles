39a4bb45e7150f5712bf372c94d66441fee96a4f
Mohit Singh <mohit@mohitsingh.in>
2022-04-30 09:28:11 +0530

    Update lua rutime locations

--- nvim/lua/lsp/servers.lua
+++ nvim/lua/lsp/servers.lua
@@ -55,16 +55,20 @@ local server_opts = {
 		}
 	end,
 	["sumneko_lua"] = function(opts)
+		local runtime_path = vim.split(package.path, ";")
+		table.insert(runtime_path, "lua/?.lua")
+		table.insert(runtime_path, "lua/?/init.lua")
 		opts.settings = {
 			Lua = {
+				runtime = {
+					version = "LuaJIT",
+					path = runtime_path,
+				},
 				diagnostics = {
 					globals = { "vim" },
 				},
 				workspace = {
-					library = {
-						[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-						[vim.fn.stdpath("config") .. "/lua"] = true,
-					},
+					library = vim.api.nvim_get_runtime_file("", true),
 				},
 				telemetry = {
 					enable = false,
