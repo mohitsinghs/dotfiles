8ce763bd34f7650f0aaee19487130f45260d72c3
Mohit Singh <mohit@mohitsingh.in>
2022-05-11 07:04:43 +0530

    Move telescope prompt to top

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,2 +1,10 @@
 local telescope = require("telescope")
+telescope.setup({
+	defaults = {
+		prompt_prefix = "   ",
+		layout_config = {
+			prompt_position = "top",
+		},
+	},
+})
 telescope.load_extension("fzf")
