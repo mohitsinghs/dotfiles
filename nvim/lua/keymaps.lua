57a16ca2034a094d534bb780cade1a5916dddd9c
Mohit Singh <mohit@mohitsingh.in>
2022-05-13 00:30:06 +0530

    Cleanup and update keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = ","
 
 local function map_normal(key, cmd)
-	vim.api.nvim_set_keymap("n", key, ":" .. cmd .. "<cr>", { noremap = true, silent = true })
+	vim.api.nvim_set_keymap("n", key, ":<C-u>" .. cmd .. "<cr>", { noremap = true, silent = true })
 end
 
 -- Write and quit
@@ -23,28 +23,29 @@ map_normal("<leader>gf", "Git push --force")
 map_normal("<leader>gu", "Git pull")
 
 --- Git Hunk
-map_normal("<leader>hu", "VGit buffer_hunk_reset")
 map_normal("<leader>hi", "VGit buffer_hunk_preview")
 map_normal("<leader>hs", "VGit buffer_hunk_stage")
+map_normal("<leader>hu", "VGit buffer_hunk_reset")
 
 -- Telescope lists
-map_normal("<space>r", "<C-u>lua require('lists.repo').list()")
-map_normal("<space>p", "<C-u>Telescope find_files")
-map_normal("<space>f", "<C-u>Telescope live_grep")
-map_normal("<space>g", "<C-u>Telescope git_commits")
-map_normal("<space>b", "<C-u>Telescope buffers")
-map_normal("<space>c", "<C-u>Telescope highlights")
-map_normal("<space>o", "<C-u>Telescope vim_options")
-map_normal("<space>h", "<C-u>Telescope help_tags")
-map_normal("<space>m", "<C-u>Telescope man_pages")
+map_normal("<space>a", "Telescope autocommands")
+map_normal("<space>b", "Telescope buffers")
+map_normal("<space>c", "Telescope commands")
 map_normal("<space>d", "Telescope diagnostics")
+map_normal("<space>f", "Telescope live_grep")
+map_normal("<space>g", "Telescope git_commits")
+map_normal("<space>h", "Telescope highlights")
+map_normal("<space>k", "Telescope keymaps")
+map_normal("<space>l", "Telescope file_browser")
+map_normal("<space>p", "Telescope find_files")
+map_normal("<space>r", "lua require('lists.repo').list()")
 map_normal("<space>s", "lua require('lists.symbols').list()")
+map_normal("<space>t", "Telescope help_tags")
+map_normal("<space>v", "Telescope vim_options")
 
 -- Some lsp specific keymaps
-map_normal("<leader>lr", "Telescope lsp_references")
 map_normal("<leader>ld", "Telescope lsp_definitions")
-map_normal("<leader>ly", "Telescope lsp_type_definitions")
 map_normal("<leader>li", "Telescope lsp_implementations")
-
--- Extra
-map_normal("<leader>n", "NvimTreeToggle")
+map_normal("<leader>lr", "Telescope lsp_references")
+map_normal("<leader>ls", "lua vim.lsp.buf.signature_help()")
+map_normal("<leader>lt", "Telescope lsp_type_definitions")
