14d561254837811e5bb0c3c1f66036c49639c9d9
Mohit Singh <mohit@mohitsingh.in>
2022-09-17 17:04:49 +0530

    fix(nvim): remove settings that are default

--- nvim/lua/config/core.lua
+++ nvim/lua/config/core.lua
@@ -3,14 +3,12 @@ local config = {
 	swapfile = false,
 	number = true,
 	relativenumber = true,
-	laststatus = 2,
 	shortmess = { a = true, I = true, F = true, W = true, c = true },
 	termguicolors = true,
 	tabstop = 2,
 	shiftwidth = 2,
 	softtabstop = 2,
 	expandtab = true,
-	foldenable = true,
 	fileencoding = "utf-8",
 	updatetime = 250,
 	signcolumn = "yes",
@@ -25,9 +23,6 @@ for key, val in pairs(config) do
 end
 
 vim.opt.clipboard:append({ "unnamedplus" })
-vim.cmd("hi Comment cterm=italic gui=italic")
 vim.cmd("color tokyonight")
-vim.cmd("set t_ZH=[3m")
-vim.cmd("set t_ZR=[23m")
 
 return config
