1ebb6c8f4e2a80ee315630c5722067643ae6e3ad
Mohit Singh <mohit@mohitsingh.in>
2022-10-19 13:01:01 +0530

    fix(nvim): move to default_capablities

--- nvim/lua/lsp/servers.lua
+++ nvim/lua/lsp/servers.lua
@@ -32,7 +32,7 @@ mason_lspconfig.setup({
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 for _, lsp in pairs(servers) do
 	local opts = {
