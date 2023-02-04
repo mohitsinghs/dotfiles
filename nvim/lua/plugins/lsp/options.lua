21ea4b22da2ae62bf4acbbe06ea82c06635e2095
Mohit Singh <mohit@mohitsingh.in>
2023-02-04 14:49:18 +0530

    fix(nvim): silence annoying checks from lua lsp

--- nvim/lua/plugins/lsp/options.lua
+++ nvim/lua/plugins/lsp/options.lua
@@ -34,6 +34,7 @@ M.settings = {
       },
       workspace = {
         library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
       },
       telemetry = {
         enable = false,
