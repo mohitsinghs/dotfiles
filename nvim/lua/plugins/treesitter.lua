c71c8b70cdb94ad6deb7676d0723d035b5dccd06
Mohit Singh <mohit@mohitsingh.in>
2023-02-09 18:40:08 +0530

    chore(nvim): remove unnecessary default keymap

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -44,12 +44,7 @@ local config = {
       clear_on_cursor_move = true,
     },
     highlight_current_scope = { enable = true },
-    smart_rename = {
-      enable = true,
-      keymaps = {
-        smart_rename = "grr",
-      },
-    },
+    smart_rename = { enable = true },
   },
 }
 
