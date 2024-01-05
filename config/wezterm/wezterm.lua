062f1ae4fbee5d304c4fd49ba7e01306aac2ee8a
Mohit Singh <mohit@mohitsingh.in>
2024-01-05 08:19:19 +0530

    wezterm: use WebGpu frontend

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -16,6 +16,7 @@ config.show_tab_index_in_tab_bar = false
 config.tab_bar_at_bottom = true
 config.tab_max_width = 24
 config.default_workspace = "home"
+config.front_end = "WebGpu"
 
 config.font = fonts.Book
 config.font_rules = {
