e8432430e8ec8889e52df415f07caabfdc58bd98
Mohit Singh <mohit@mohitsingh.in>
2024-07-03 06:23:34 +0530

    fix(nvim): remove comments plugin and use builtin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,6 +1,5 @@
 return {
   { "kylechui/nvim-surround", event = "BufReadPost", config = true },
-  { "numToStr/Comment.nvim", event = "BufReadPost", config = true },
   {
     "norcalli/nvim-colorizer.lua",
     opts = { "*" },
