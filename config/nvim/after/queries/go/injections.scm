955d32f6989a6420539fc72daa3f18b20998923d
Mohit Singh <mohit@mohitsingh.in>
2025-04-03 09:47:45 +0530

    fix(nvim): broken highlight for embedded sql in go

--- config/nvim/after/queries/go/injections.scm
+++ config/nvim/after/queries/go/injections.scm
@@ -1,10 +1,10 @@
 ; extends
 
 ([
-  (interpreted_string_literal)
-  (raw_string_literal)
+  (interpreted_string_literal_content)
+  (raw_string_literal_content)
   ] @injection.content
   (#match? @injection.content "(SELECT|select|INSERT|insert|UPDATE|update|DELETE|delete).+(FROM|from|INTO|into|VALUES|values|SET|set).*(WHERE|where|GROUP BY|group by)?")
-  (#offset! @injection.content 0 1 0 -1)
+  (#offset! @injection.content 0 0 0 0)
   (#set! injection.language "sql")
 )
